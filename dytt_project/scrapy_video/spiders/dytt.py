cca9a4216b0901dcf762f53ba1c30397313fbd0c
liushangliang <phenix3443@gmail.com>
2018-11-13 10:34:13 +0800

    创建项目

--- dytt_project/scrapy_video/spiders/dytt.py
+++ dytt_project/scrapy_video/spiders/dytt.py
@@ -3,25 +3,35 @@ import scrapy
 import json
 import re
 
+
 class DyttSpider(scrapy.Spider):
     name = 'dytt'
-    allowed_domains = ['www.dytt8.net']
-    # start_urls = ['http://www.dytt8.net/']
-    start_urls = ["https://www.dytt8.net/html/gndy/dyzz/20181102/57714.html"]
+    allowed_domains = ['www.dytt8.net', 'www.ygdy8.net']
 
-    # def parse(self, response):
-    #     """分析首页"""
-    #     # 最新电影
-    #     latest_moive_link = response.xpath('//*[@id="menu"]/div/ul/li[1]/a')
+    start_urls = ['http://www.dytt8.net/']
+
+    def parse(self, response):
+        """分析首页"""
+        # 最新电影
+        latest_moive_link = response.xpath(
+            '//*[@id="menu"]/div/ul/li[1]/a/@href').extract_first()
+        yield response.follow(
+            latest_moive_link, callback=self.parse_latest_movie)
 
     def parse_latest_movie(self, response):
         """抓取最新电影列表"""
         # 最新电影列表
-        movie_list = response.xpath(
-            '//*[@id="header"]/div/div[3]/div[3]/div[2]/div[2]/div[2]/ul')
+        movie_list = response.xpath('//div[@class="co_content8"]')
+        detail_links = movie_list.xpath(
+            './ul//a[@class="ulink"]/@href').extract()
 
-    # def parse_moive_detail(self, response):
-    def parse(self, response):        
+        for dl in detail_links:
+            yield response.follow(dl, callback=self.parse_moive_detail)
+
+        # next_page = movie_list.xpath('./div//a/@href').extract_first()
+        # yield response.follow(next_page, self.parse)
+
+    def parse_moive_detail(self, response):
         """抓取电影详细信息"""
         movie = dict()
         info = response.xpath('//*[@id="Zoom"]')
@@ -64,28 +74,39 @@ class DyttSpider(scrapy.Spider):
                     movie[v] = s[len(k):]
                     break
 
-        movie["translation"] = [s.strip() for s in movie["translation"].split("/")]
+        movie["translation"] = [
+            s.strip() for s in movie["translation"].split("/")
+        ]
         movie["title"] = [s.strip() for s in movie["title"].split("/")]
-        movie["origin_area"] = [s.strip() for s in movie["origin_area"].split("/")]
+        movie["origin_area"] = [
+            s.strip() for s in movie["origin_area"].split("/")
+        ]
         movie["category"] = [s.strip() for s in movie["category"].split("/")]
         movie["lang"] = [s.strip() for s in movie["lang"].split("/")]
         movie["premiere"] = [s.strip() for s in movie["premiere"].split("/")]
-        movie["tags"] = [s.strip() for s in movie["tags"].split("|")] if "tags" in movie else []
+        movie["tags"] = [s.strip() for s in movie["tags"].split("|")
+                         ] if "tags" in movie else []
         movie["actors"] = [movie["actors"]]
 
         # 没有特殊标记的纯文本
         pure_texts = {s.strip() for s in texts if ("◎" not in s and s.strip())}
         # 获奖的文本
-        prize_texts = {s for s in pure_texts if re.search(r"百想艺术大赏|青龙奖|电影大奖|技术奖|金狮奖|电影节|金马|金鹿奖|长春电影节|电影奖", s)}
-        print(prize_texts)
+        prize_texts = {
+            s
+            for s in pure_texts if re.search(
+                r"百想艺术大赏|青龙奖|电影大奖|技术奖|金狮奖|电影节|金马|金鹿奖|长春电影节|电影奖|主竞赛单元|最佳", s)
+        }
         # 剩余的演员
         for s in pure_texts.difference(prize_texts):
             if len(s) < 50 and (not re.search("改编自|最佳", s)):
                 movie["actors"].append(s)
-        
+
         # 简介
-        movie["Introduction"] = "\n".join([s.strip() for s in set.difference(pure_texts,prize_texts,set(movie["actors"]))])
+        movie["Introduction"] = "\n".join([
+            s.strip() for s in set.difference(pure_texts, prize_texts,
+                                              set(movie["actors"]))
+        ])
         # 获奖
         movie["prize"] = list(prize_texts)
-        
+
         print(json.dumps(movie, indent=4, ensure_ascii=False))
